6fdca559ed852f26b873d36091ee76ff877d5661
unknown <colost72@gmail.com>
2022-06-27 12:50:24 -0600

    Created Player class

---
+++ player.py
@@ -0,0 +1,11 @@
+# parent Player class #
+
+class Player:
+
+    def __init__(self):
+        self.gestures = str["rock", "paper", "scissors", "lizard", "spock"]
+        self.chosen_gesture()
+        self.win = 0
+        
+
+
